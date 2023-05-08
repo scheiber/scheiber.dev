a81009c5dc30e003739fca8a97b75b90a80f261e
Jonathan Scheiber <jonathan@scheiber.dev>
2023-05-08 17:15:25 -0400

    add onClick to open in new tab and description

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -71,6 +71,10 @@ const backgrounds = [
   },
 ];
 
+const onClick = (event) => {
+  window.open(event.target.src, "_blank");
+};
+
 const GalleryDisplay = () => (
   <div className="gallery">
     <HelmetProvider>
@@ -79,9 +83,16 @@ const GalleryDisplay = () => (
       </Helmet>
     </HelmetProvider>
     <h1 className="title">Gallery</h1>
+    <div className="now-text">
+      <p>
+        This is a collection of graphics and illustrations that I have created
+        during the course of my career. Click the images below to open them in a
+        new tab.
+      </p>
+    </div>
     <div className="gallery-grid">
       {" "}
-      <Gallery photos={backgrounds} />
+      <Gallery photos={backgrounds} onClick={onClick} />
     </div>
   </div>
 );
