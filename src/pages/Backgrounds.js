7f7ae9aa3a6d50b7116dbe4ee88eddf65bca626f
Jonathan Scheiber <jonathan@scheiber.dev>
2023-05-08 17:18:21 -0400

    move backgrounds to seperate file

---
+++ src/pages/Backgrounds.js
@@ -0,0 +1,70 @@
+const backgrounds = [
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2022_Black.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2022_Blue.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2022_Gray.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2023_Black.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2023_Blue.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2023_Gray.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Pursuit_AAPI.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Pursuit_Latin_Heritage_Month_EN.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Pursuit_Latin_Heritage_Month_ES.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Pursuit_Black_History_Month.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+  {
+    src: "/images/backgrounds/Jonathan_Scheiber_Pursuit_Pride.png",
+    width: 480,
+    height: 270,
+    className: "tile",
+  },
+];
+
+export default backgrounds;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,75 +1,7 @@
 import "./Gallery.css";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import Gallery from "react-photo-gallery";
-
-const backgrounds = [
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2022_Black.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2022_Blue.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2022_Gray.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2023_Black.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2023_Blue.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Instructor_Appreciation_Week_2023_Gray.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Pursuit_AAPI.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Pursuit_Latin_Heritage_Month_EN.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Pursuit_Latin_Heritage_Month_ES.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Pursuit_Black_History_Month.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-  {
-    src: "/images/backgrounds/Jonathan_Scheiber_Pursuit_Pride.png",
-    width: 480,
-    height: 270,
-    className: "tile",
-  },
-];
+import backgrounds from "./Backgrounds";
 
 const onClick = (event) => {
   window.open(event.target.src, "_blank");
