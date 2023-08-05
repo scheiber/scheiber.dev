a989c04ba751fb79eab946da8fdc32022b995e2e
Jonathan Scheiber <jonathan@scheiber.dev>
2023-08-05 19:05:06 -0400

    update now page

--- src/pages/Now.js
+++ src/pages/Now.js
@@ -20,11 +20,19 @@ const About = () => (
           /now page movement
         </a>
         . The purpose of this page is to share what I am currently focused on at
-        this point in their life.
+        this point in my life.
       </p>
       <h1 className="subheader">Learning</h1>
+      <a href="https://www.coursera.org/">
+        <img
+          className="logo"
+          src="/images/logos/coursera.png"
+          alt="Coursera logo"
+          height="50px"
+        />
+      </a>
       <p>
-        I'm currently completing professional certificate programs through{" "}
+        I am currently completing professional certificate programs through{" "}
         <a className="text-link" href="https://www.coursera.org/">
           Coursera
         </a>{" "}
@@ -34,13 +42,52 @@ const About = () => (
           href="https://www.coursera.org/professional-certificates/google-ux-design"
         >
           Google UX Design
-        </a>{" "}
-        and{" "}
+        </a>
+        ,{" "}
         <a
           className="text-link"
           href="https://www.coursera.org/professional-certificates/google-it-support"
         >
           Google IT Support
+        </a>
+        , and{" "}
+        <a
+          className="text-link"
+          href="https://www.coursera.org/professional-certificates/microsoft-cybersecurity-analyst"
+        >
+          Microsoft Cybersecurity Analyst
+        </a>
+        .
+      </p>
+      <h1 className="subheader">Watching</h1>
+      <a href="https://www.last.fm/user/JonScheiber">
+        <img
+          className="logo"
+          src="/images/logos/letterboxd.png"
+          alt="Letterboxd logo"
+          height="50px"
+        />
+      </a>
+      <p>
+        Track what movies I've watched on{" "}
+        <a className="text-link" href="https://letterboxd.com/js/">
+          Letterboxd
+        </a>
+        .
+      </p>
+      <h1 className="subheader">Listening</h1>
+      <a href="https://www.last.fm/user/JonScheiber">
+        <img
+          className="logo"
+          src="/images/logos/lastfm.png"
+          alt="Last.fm logo"
+          height="40px"
+        />
+      </a>
+      <p>
+        Track what music I'm listening to on{" "}
+        <a className="text-link" href="https://www.last.fm/user/JonScheiber">
+          Last.fm
         </a>
         .
       </p>
